35305a828dcc80c7f8148fe49c726fb00856c876
Keith Hunter <keith@abstract.com>
2019-12-08 13:03:25 -0500

    day 5 part 2

--- day5/intcode.go
+++ day5/intcode.go
@@ -26,10 +26,24 @@ func executeInput() {
 		instructions = append(instructions, instruction)
 	}
 
-	ExecuteInstructions(1, instructions, func(out int) {
+	ExecuteInstructions(5, instructions, func(out int) {
 		fmt.Printf("%v\n", out)
 	})
 }
+
+type OpCode int
+
+const (
+	OpCodeAdd         OpCode = 1
+	OpCodeMultiply    OpCode = 2
+	OpCodeInput       OpCode = 3
+	OpCodeOutput      OpCode = 4
+	OpCodeJumpIfTrue  OpCode = 5
+	OpCodeJumpIfFalse OpCode = 6
+	OpCodeLessThan    OpCode = 7
+	OpCodeEquals      OpCode = 8
+	OpCodeEnd         OpCode = 99
+)
 
 type Mode int
 
@@ -50,11 +64,9 @@ Loop:
 	for {
 		param := newParam(c[i])
 		length := 4
-		// fmt.Printf("params: %v\n", param)
 
 		switch param.opCode {
-		case 1:
-			// fmt.Printf("instr: %v\n", c[i:i+4])
+		case OpCodeAdd:
 			operand1 := 0
 			if param.op1Mode == ModeImmediate {
 				operand1 = c[i+1]
@@ -74,8 +86,8 @@ Loop:
 				panic(errors.New("unexpected immediate"))
 			}
 			c[resultPosition] = operand1 + operand2
-		case 2:
-			// fmt.Printf("instr: %v\n", c[i:i+4])
+
+		case OpCodeMultiply:
 			operand1 := 0
 			if param.op1Mode == ModeImmediate {
 				operand1 = c[i+1]
@@ -83,7 +95,7 @@ Loop:
 				operand1 = c[c[i+1]]
 			}
 
-			operand2 := c[c[i+2]]
+			operand2 := 0
 			if param.op2Mode == ModeImmediate {
 				operand2 = c[i+2]
 			} else {
@@ -95,24 +107,123 @@ Loop:
 			}
 			resultPosition := c[i+3]
 			c[resultPosition] = operand1 * operand2
-		case 3:
-			// fmt.Printf("instr: %v\n", c[i:i+2])
+
+		case OpCodeInput:
 			if param.op1Mode == ModeImmediate {
 				panic(errors.New("unexpected immediate"))
 			}
 			length = 2
 			position := c[i+1]
 			c[position] = input
-		case 4:
-			// fmt.Printf("instr: %v\n", c[i:i+2])
-			// if param.op1Mode == ModeImmediate {
-			// 	panic(errors.New("unexpected immediate"))
-			// }
+
+		case OpCodeOutput:
 			length = 2
-			position := c[i+1]
-			outputCallback(c[position])
-		case 99:
+
+			value := 0
+			if param.op1Mode == ModeImmediate {
+				value = c[i+1]
+			} else {
+				value = c[c[i+1]]
+			}
+			outputCallback(value)
+
+		case OpCodeJumpIfTrue:
+			operand1 := 0
+			if param.op1Mode == ModeImmediate {
+				operand1 = c[i+1]
+			} else {
+				operand1 = c[c[i+1]]
+			}
+
+			operand2 := 0
+			if param.op2Mode == ModeImmediate {
+				operand2 = c[i+2]
+			} else {
+				operand2 = c[c[i+2]]
+			}
+
+			if operand1 != 0 {
+				i = operand2
+				continue
+			}
+			length = 3
+
+		case OpCodeJumpIfFalse:
+			operand1 := 0
+			if param.op1Mode == ModeImmediate {
+				operand1 = c[i+1]
+			} else {
+				operand1 = c[c[i+1]]
+			}
+
+			operand2 := 0
+			if param.op2Mode == ModeImmediate {
+				operand2 = c[i+2]
+			} else {
+				operand2 = c[c[i+2]]
+			}
+
+			if operand1 == 0 {
+				i = operand2
+				continue
+			}
+			length = 3
+
+		case OpCodeLessThan:
+			operand1 := 0
+			if param.op1Mode == ModeImmediate {
+				operand1 = c[i+1]
+			} else {
+				operand1 = c[c[i+1]]
+			}
+
+			operand2 := 0
+			if param.op2Mode == ModeImmediate {
+				operand2 = c[i+2]
+			} else {
+				operand2 = c[c[i+2]]
+			}
+
+			if param.op3Mode == ModeImmediate {
+				panic(errors.New("unexpected immediate"))
+			}
+			resultPosition := c[i+3]
+
+			if operand1 < operand2 {
+				c[resultPosition] = 1
+			} else {
+				c[resultPosition] = 0
+			}
+
+		case OpCodeEquals:
+			operand1 := 0
+			if param.op1Mode == ModeImmediate {
+				operand1 = c[i+1]
+			} else {
+				operand1 = c[c[i+1]]
+			}
+
+			operand2 := 0
+			if param.op2Mode == ModeImmediate {
+				operand2 = c[i+2]
+			} else {
+				operand2 = c[c[i+2]]
+			}
+
+			if param.op3Mode == ModeImmediate {
+				panic(errors.New("unexpected immediate"))
+			}
+			resultPosition := c[i+3]
+
+			if operand1 == operand2 {
+				c[resultPosition] = 1
+			} else {
+				c[resultPosition] = 0
+			}
+
+		case OpCodeEnd:
 			break Loop
+
 		default:
 			panic(errors.Errorf("unexpected param %v, instr %v", param, c[i]))
 		}
@@ -127,7 +238,7 @@ Loop:
 }
 
 type parameter struct {
-	opCode  int
+	opCode  OpCode
 	op1Mode Mode
 	op2Mode Mode
 	op3Mode Mode
@@ -144,10 +255,11 @@ func newParam(input int) parameter {
 	if len >= 2 {
 		opCode = str[len-2:]
 	}
-	p.opCode, err = strconv.Atoi(opCode)
+	code, err := strconv.Atoi(opCode)
 	if err != nil {
 		panic(errors.Wrap(err, "cannot convert str "+opCode))
 	}
+	p.opCode = OpCode(code)
 
 	if len >= 3 {
 		op1 := string(str[len-3])
--- day5/intcode_test.go
+++ day5/intcode_test.go
@@ -60,9 +60,181 @@ func TestExample6(t *testing.T) {
 	assert.Equal(t, []int{1002, 4, 3, 4, 99}, result)
 }
 
+// 3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the 
+// input is equal to 8; output 1 (if it is) or 0 (if it is not).
+func TestExample7(t *testing.T) {
+	t.Run("input 1", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(1, []int{3,9,8,9,10,9,4,9,99,-1,8}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,9,8,9,10,9,4,9,99,0,8}, result)
+		assert.Equal(t, []int{0}, output)
+	})
+
+	t.Run("input 8", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(8, []int{3,9,8,9,10,9,4,9,99,-1,8}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,9,8,9,10,9,4,9,99,1,8}, result)
+		assert.Equal(t, []int{1}, output)
+	})
+}
+
+// 3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the 
+// input is less than 8; output 1 (if it is) or 0 (if it is not).
+func TestExample8(t *testing.T) {
+	t.Run("input 1", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(1, []int{3,9,7,9,10,9,4,9,99,-1,8}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,9,7,9,10,9,4,9,99,1,8}, result)
+		assert.Equal(t, []int{1}, output)
+	})
+
+	t.Run("input 8", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(8, []int{3,9,7,9,10,9,4,9,99,-1,8}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,9,7,9,10,9,4,9,99,0,8}, result)
+		assert.Equal(t, []int{0}, output)
+	})
+}
+
+// 3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the 
+// input is equal to 8; output 1 (if it is) or 0 (if it is not).
+func TestExample9(t *testing.T) {
+	t.Run("input 1", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(1, []int{3,3,1108,-1,8,3,4,3,99}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,3,1108,0,8,3,4,3,99}, result)
+		assert.Equal(t, []int{0}, output)
+	})
+
+	t.Run("input 8", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(8, []int{3,3,1108,-1,8,3,4,3,99}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,3,1108,1,8,3,4,3,99}, result)
+		assert.Equal(t, []int{1}, output)
+	})
+}
+
+// 3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the 
+// input is equal to 8; output 1 (if it is) or 0 (if it is not).
+func TestExample10(t *testing.T) {
+	t.Run("input 1", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(1, []int{3,3,1107,-1,8,3,4,3,99}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,3,1107,1,8,3,4,3,99}, result)
+		assert.Equal(t, []int{1}, output)
+	})
+
+	t.Run("input 8", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(8, []int{3,3,1107,-1,8,3,4,3,99}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,3,1107,0,8,3,4,3,99}, result)
+		assert.Equal(t, []int{0}, output)
+	})
+}
+
+/*
+Here are some jump tests that take an input, then output 0 if the input was 
+zero or 1 if the input was non-zero:
+
+3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 (using position mode)
+3,3,1105,-1,9,1101,0,0,12,4,12,99,1 (using immediate mode)
+*/
+func TestExample11(t *testing.T) {
+	t.Run("instr 6: input 1", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(1, []int{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,12,6,12,15,1,13,14,13,4,13,99,1,1,1,9}, result)
+		assert.Equal(t, []int{1}, output)
+	})
+
+	t.Run("instr 6: input 0", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(0, []int{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,12,6,12,15,1,13,14,13,4,13,99,0,0,1,9}, result)
+		assert.Equal(t, []int{0}, output)
+	})
+
+	t.Run("instr 5: input 1", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(1, []int{3,3,1105,-1,9,1101,0,0,12,4,12,99,1}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,3,1105,1,9,1101,0,0,12,4,12,99,1}, result)
+		assert.Equal(t, []int{1}, output)
+	})
+
+	t.Run("instr 5: input 0", func(t *testing.T) {
+		output := []int{}
+		result := ExecuteInstructions(0, []int{3,3,1105,-1,9,1101,0,0,12,4,12,99,1}, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{3,3,1105,0,9,1101,0,0,12,4,12,99,0}, result)
+		assert.Equal(t, []int{0}, output)
+	})
+}
+
+/*
+3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,
+125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+
+The above example program uses an input instruction to ask for a single number. 
+The program will then output 999 if the input value is below 8, output 1000 if 
+the input value is equal to 8, or output 1001 if the input value is greater 
+than 8.
+*/
+func TestExample12(t *testing.T) {
+	input := []int{3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99}
+
+	t.Run("input 1", func(t *testing.T) {
+		t.SkipNow()
+		output := []int{}
+		ExecuteInstructions(1, input, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{999}, output)
+	})
+
+	t.Run("input 8", func(t *testing.T) {
+		output := []int{}
+		ExecuteInstructions(8, input, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{1000}, output)
+	})
+
+	t.Run("input 9", func(t *testing.T) {
+		t.SkipNow()
+		output := []int{}
+		ExecuteInstructions(8, input, func(out int) {
+			output = append(output, out)
+		})
+		assert.Equal(t, []int{1001}, output)
+	})
+}
+
 func TestNewParam(t *testing.T) {
 	p := newParam(1002)
-	assert.Equal(t, p.opCode, 2)
+	assert.Equal(t, p.opCode, OpCodeMultiply)
 	assert.Equal(t, p.op1Mode, ModePosition)
 	assert.Equal(t, p.op2Mode, ModeImmediate)
 	assert.Equal(t, p.op3Mode, ModePosition)
